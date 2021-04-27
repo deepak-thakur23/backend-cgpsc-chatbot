e5390b52dcb33a51dfe89d5d551a41db97d8b46d
deepak-thakur23 <deepak.thakur23@gmail.com>
2021-04-27 20:41:49 +0530

    initial commit

--- server.js
+++ server.js
@@ -23,3 +23,5 @@ require('./config/passport')(passport);
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+// "cross-env NODE_ENV=production node server",
+//     "dev": "cross-env NODE_ENV=development nodemon server"
