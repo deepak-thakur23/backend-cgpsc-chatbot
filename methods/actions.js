19e675450e31458d7ff51af61c90e74ea12a31a5
deepak-thakur23 <deepak.thakur23@gmail.com>
2021-04-29 18:55:54 +0530

    initial commit

--- methods/actions.js
+++ methods/actions.js
@@ -80,13 +80,13 @@ var functions = {
         if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
             var token = req.headers.authorization.split(' ')[1];
             var decodedtoken = jwt.decode(token, config.secret);
-            return res.json({
+            return res.status(200).send({
                 success: true,
                 massage: 'Hello, ' + decodedtoken.name
             })
         }
         else {
-            return res.json({
+            return res.status(403).send({
                 success: false,
                 massage: 'No headers..!'
             })
